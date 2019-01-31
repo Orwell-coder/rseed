33ea5d4fc5f38a60f5a0f182aa3fd1d6067e0edf
zhengxu <zhengxu@zxisl.com>
2019-01-31 11:25:18 +0800

    add template file of custom path

--- src/Rseed.php
+++ src/Rseed.php
@@ -37,6 +37,14 @@ class Rseed extends Iseed
     private $path = null;
 
 
+    /**
+     * DatabaseSeeder file save floder
+     *
+     * @var [type]
+     */
+    private $seederPath = null;
+
+
   
     /**
      * build a seeder file
@@ -100,6 +108,78 @@ class Rseed extends Iseed
     {
         $this->path = $path;
         return $this;
+    }
+
+    /**
+     * Get DatabaseSeeder file name
+     *
+     * @return void
+     */
+    public function getDatabaseSeederPath()
+    {
+        return $this->seederPath ?? base_path() . config('iseed::config.path') . '/DatabaseSeeder.php';
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $path
+     * @return void
+     */
+    public function setDatabaseSeederPath($path)
+    {
+        try{
+            if(!$this->path){
+                throw new \Exception("Please set SeedPath floder !");
+            }
+            copy(__DIR__ . DIRECTORY_SEPARATOR . 'template'. DIRECTORY_SEPARATOR . 'DatabaseSeeder.php', $this->path);
+            $this->seederPath = $this->path;
+            return $this;
+        }catch(\Exception $e){
+            throw new \Exception($e->getMessage());
+        }
+        
+    }
+
+        /**
+     * Cleans the iSeed section
+     * @return bool
+     */
+    public function cleanSection()
+    {
+        $databaseSeederPath = $this->getDatabaseSeederPath();
+
+        $content = $this->files->get($databaseSeederPath);
+
+        $content = preg_replace("/(\#iseed_start.+?)\#iseed_end/us", "#iseed_start\n\t\t#iseed_end", $content);
+
+        return $this->files->put($databaseSeederPath, $content) !== false;
+        return false;
+    }
+
+    /**
+     * Updates the DatabaseSeeder file's run method (kudoz to: https://github.com/JeffreyWay/Laravel-4-Generators)
+     * @param  string  $className
+     * @return bool
+     */
+    public function updateDatabaseSeederRunMethod($className)
+    {
+        $databaseSeederPath = $this->getDatabaseSeederPath();
+
+        $content = $this->files->get($databaseSeederPath);
+        if (strpos($content, "\$this->call({$className}::class)") === false) {
+            if (
+                strpos($content, '#iseed_start') &&
+                strpos($content, '#iseed_end') &&
+                strpos($content, '#iseed_start') < strpos($content, '#iseed_end')
+            ) {
+                $content = preg_replace("/(\#iseed_start.+?)(\#iseed_end)/us", "$1\$this->call({$className}::class);{$this->newLineCharacter}{$this->indentCharacter}{$this->indentCharacter}$2", $content);
+            } else {
+                $content = preg_replace("/(run\(\).+?)}/us", "$1{$this->indentCharacter}\$this->call({$className}::class);{$this->newLineCharacter}{$this->indentCharacter}}", $content);
+            }
+        }
+
+        return $this->files->put($databaseSeederPath, $content) !== false;
     }
 
    
